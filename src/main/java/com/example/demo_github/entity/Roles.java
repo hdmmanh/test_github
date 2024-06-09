9c437bcbf80178b00ce1ad5db87e781d04b802fa
hdmmanh <hdmmanh26@gmail.com>
2024-06-09 21:16:03 +0700

    push code

---
+++ src/main/java/com/example/demo_github/entity/Roles.java
@@ -0,0 +1,4 @@
+package com.example.demo_github.entity;
+
+public class Roles {
+}
--- src/main/java/com/example/demo_github/entity/user.java
+++ src/main/java/com/example/demo_github/entity/user.java
@@ -3,6 +3,5 @@ package com.example.demo_github.entity;
 public class user {
     private String userName;
     private String password;
-
     private String role;
 }
